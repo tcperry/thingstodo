ec9daeb2b6326aac1093a93361b970269df6f9a3
jessicapierre23 <jessica.pierre23@gmail.com>
2014-04-13 20:31:29 -0400

    commit
    commit

---
+++ addlistitems.js
@@ -0,0 +1,64 @@
+
+$(document).ready(function() {
+
+
+
+
+
+$('form').submit(function(e){
+    e.preventDefault();
+	addItems();
+});
+
+$('input').keypress(function(e){
+    if(e.keyCode == 13){
+	    e.preventDefault();
+        addItems();
+    }
+});
+
+  
+
+
+ });
+
+
+
+	
+
+
+function addItems() {
+
+var listform = document.getElementById("myList");
+var input = listform.elements[0].value; //input from text box
+
+if (/^\s+/.test(input)|| input == "") 
+   alert("Please enter an item and do not include spaces before the item.");
+   
+else
+
+
+$('#livelist').append("<li><input type='checkbox'/><label>" + input + "</label></li>");
+
+$("#itembox").val("");
+
+$('li').dblclick(function() {
+      
+      $(this).fadeOut('fast');
+	  
+	  
+  });
+  
+  $(function() {
+
+
+ $( ".sortable" ).sortable();
+
+ $( ".sortable" ).disableSelection();
+
+ });
+  
+
+
+} //end addItems function
+
